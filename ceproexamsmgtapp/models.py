db7b70ceddf48b0361f671dc6fdb23ddd65f9dca
LucileP <lucile.pinard@gmail.com>
2023-05-11 08:41:21 +0200

    Part 2 Model

--- ceproexamsmgtapp/models.py
+++ ceproexamsmgtapp/models.py
@@ -17,9 +17,20 @@ class ExamStatus(db.Model):
     code = db.Column(db.String(20))
     name = db.Column(db.String(50))
 
+class ServiceLevel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(20))
+    name = db.Column(db.String(50))
+
+class Service(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(20))
+    description = db.Column(db.String(100))
+
+
 class Exam(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(10))
+    code = db.Column(db.String(20))
     name = db.Column(db.String(50))
     exam_date = db.column(db.DateTime)
     exam_years = db.column(db.string(10))
@@ -31,11 +42,34 @@ class Exam(db.Model):
     remark = db.column(db.string(250))
     exam_type = db.relationship('ExamType', backref='exams', lazy='dynamic')
     exam_status = db.relationship('ExamStatus', backref='exams', lazy='dynamic')
+    service_level = db.relationship('ServiceLevel', backref='exams', lazy='dynamic')
+    service = db.relationship('Service', backref='exams', lazy='dynamic')
+
+class EmployeeType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(20))
+    name = db.Column(db.String(20))
+
+class Employee(db.Model):
+    id =db.Column(db.Integer, primary_key=True)
+    lastname = db.Column(db.String(50))
+    firstname = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    sciper = db.Column(db.String(6))
+    emplyee_type = db.relationship('EmployeeType', backref='employee', lazy='dynamic')
+
+class EmployeeHasExam(db.Model):
+    id=db.column(db.Integer, primary_key=True)
+    exam = db.relationship('Exams', backref='EmployeeHasExam', lazy='dynamic', nullable=False)
+    employee = db.relatonship('Employee', backref='EmployeeHasExam', lazy='dynamic', nullable=False)
 
 class ExamHasSection(db.Model):
     id=db.column(db.Integer, primary_key=True)
-    section = db.relationship('Section', backref='ExamHasSections', lazy='dynamic', nullable=False)
-    exam = db.relationship('Exams', backref='ExamHasSections', lazy='dynamic', nullable=False)
+    section = db.relationship('Section', backref='ExamHasSection', lazy='dynamic', nullable=False)
+    exam = db.relationship('Exam', backref='ExamHasSection', lazy='dynamic', nullable=False)
+
+
+
 
     # ExamStatus
     #   id = 1
