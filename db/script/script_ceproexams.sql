337ca360e540d87d17231b23346020e4c094751d
Ludovic Bonivento <ludovic.bonivento@epfl.ch>
2023-06-22 16:04:10 +0200

    update db scripts

--- db/script/script_ceproexams.sql
+++ db/script/script_ceproexams.sql
@@ -1,6 +1,5 @@
 DROP DATABASE IF EXISTS ceproexamsmgt;
 CREATE DATABASE ceproexamsmgt CHARACTER SET utf8mb4 collate utf8mb4_bin;
-DROP TABLE IF EXISTS ceproexamsmgt.exam_has_section;
 DROP TABLE IF EXISTS ceproexamsmgt.user_has_exam;
 DROP TABLE IF EXISTS ceproexamsmgt.user;
 DROP TABLE IF EXISTS ceproexamsmgt.user_type;
@@ -59,42 +58,46 @@ CREATE TABLE ceproexamsmgt.user
   INDEX fk_user_type_idx (user_type_id ASC),
   CONSTRAINT fk_user_user_type_id FOREIGN KEY (user_type_id) REFERENCES user_type (id)
 );
+
+CREATE TABLE ceproexamsmgt.academic_year
+(
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	code VARCHAR(45) NULL,
+	name VARCHAR(100) NULL 
+);
+
 CREATE TABLE ceproexamsmgt.exam
 (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  code VARCHAR(20),
-  name VARCHAR(50),
+  code VARCHAR(30),
+  name VARCHAR(100),
   service_level_id INT,
   service_id INT,
   exam_type_id INT,
   exam_status_id INT,
   exam_date DATETIME,
-  exam_years VARCHAR (10),
+  academic_year_id INT,
   exam_semester VARCHAR (10),
   nb_students INT,
   nb_pages INT,
   deadline_prep DATETIME,
   deadline_repro DATETIME,
-  remark VARCHAR (250),
+  remark TEXT,
+  section_id INT,
   INDEX fk_service_level_idx (service_level_id ASC),
   INDEX fk_services_idx (service_id ASC),
   INDEX fk_exam_type_idx (exam_type_id ASC),
   INDEX fk_exam_status_idx (exam_status_id ASC),
+  INDEX fk_exam_academic_year_idx (academic_year_id ASC),
+  INDEX fk_exam_section_idx (section_id ASC),
   CONSTRAINT fk_exam_service_level FOREIGN KEY (service_level_id) REFERENCES service_level (id),
   CONSTRAINT fk_exam_service FOREIGN KEY (service_id) REFERENCES service (id),
   CONSTRAINT fk_exam_exam_type FOREIGN KEY (exam_type_id) REFERENCES exam_type (id),
-  CONSTRAINT fk_exam_exam_status FOREIGN KEY (exam_status_id) REFERENCES exam_status (id)
+  CONSTRAINT fk_exam_exam_status FOREIGN KEY (exam_status_id) REFERENCES exam_status (id),
+  CONSTRAINT fk_exam_academic_year FOREIGN KEY (academic_year_id) REFERENCES academic_year (id),
+  CONSTRAINT fk_exam_section FOREIGN KEY (section_id) REFERENCES section (id)
     );
-CREATE TABLE ceproexamsmgt.exam_has_section
-(
-  exam_id INT NOT NULL,
-  section_id INT NOT NULL,
-  PRIMARY KEY (exam_id, section_id),
-  INDEX fk_section_idx (section_id ASC),
-  INDEX fk_exam_idx (exam_id ASC),
-  CONSTRAINT fk_exam_has_section_exam_id FOREIGN KEY (exam_id) REFERENCES exam (id),
-  CONSTRAINT fk_exam_has_section_section_id FOREIGN KEY (section_id) REFERENCES section (id)
-);
+    
 CREATE TABLE ceproexamsmgt.user_has_exam
 (
   user_id INT NOT NULL,
